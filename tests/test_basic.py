63c4d5a7d27175bb7849f7a37fe6fa6455afaaa6
Ilyas Ngou <127138284+IlyasNgou@users.noreply.github.com>
2025-04-07 11:51:27 +0100

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,17 @@
 # tests/test_basic.py
 
-from EPL_SCRAPER.main import extract_matches
+import sys
+import os
+
+# Add the project directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+
+from main import extract_matches
 
 def test_extract_matches_runs():
     try:
-        extract_matches(1)  
+        extract_matches(1)  =
         assert True
     except Exception as e:
         assert False, f"Script failed with error: {e}"
